35923980a51cad1aa0d6e39972e066731d3e8779
Kostas <chaviarask02@gmail.com>
2015-12-16 20:08:54 +0200

    Import the project at the GitHub

---
+++ app/src/main/java/com/havistudio/popularmovies/MainActivityFragment.java
@@ -0,0 +1,31 @@
+package com.havistudio.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // spinner
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.order_movies);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.orderby_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        return rootView;
+    }
+}
